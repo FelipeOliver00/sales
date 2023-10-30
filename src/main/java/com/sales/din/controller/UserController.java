a3fe3b4cdca4c0e2cb289e19eb3a1ce3be63e706
Felipe <felipecastro0208@hotmail.com>
2023-10-30 16:24:18 -0300

    Created User endpoints how put and delete

--- src/main/java/com/sales/din/controller/UserController.java
+++ src/main/java/com/sales/din/controller/UserController.java
@@ -6,10 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/user")
@@ -35,4 +34,26 @@ public class UserController {
         }
     }
 
+    @PutMapping
+    public ResponseEntity put(@RequestBody User user) {
+        Optional<User> userToEdit = userRepository.findById(user.getId());
+
+        if (userToEdit.isPresent()) {
+            userRepository.save(user);
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable long id) {
+
+        try {
+            userRepository.deleteById(id);
+            return new ResponseEntity<>("Usuário removido com sucesso!", HttpStatus.OK);
+        } catch (Exception error) {
+            return new ResponseEntity<>(error.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
