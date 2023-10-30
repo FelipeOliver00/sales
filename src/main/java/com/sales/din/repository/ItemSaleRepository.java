84e3202f6cb3abacc99ea6094b684678fd88f623
Felipe <felipecastro0208@hotmail.com>
2023-10-30 12:17:11 -0300

    Created repositories to entities

---
+++ src/main/java/com/sales/din/repository/ItemSaleRepository.java
@@ -0,0 +1,9 @@
+package com.sales.din.repository;
+
+import com.sales.din.entity.ItemSale;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemSaleRepository extends JpaRepository<ItemSale, Long> {
+}
