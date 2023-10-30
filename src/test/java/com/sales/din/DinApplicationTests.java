8e9852c1c7b7657243713b4e8b579a86b239e8c9
Felipe <felipecastro0208@hotmail.com>
2023-10-30 11:12:27 -0300

    Initial project

---
+++ src/test/java/com/sales/din/DinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sales.din;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
