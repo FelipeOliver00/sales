44e2b5d0ccabbb7ad307f53a9857190246024fc4
Felipe <felipecastro0208@hotmail.com>
2023-10-30 12:41:24 -0300

    Refactored interface name and dialect

--- src/main/java/com/sales/din/repository/ProductRepository.java
+++ src/main/java/com/sales/din/repository/ProductRepository.java
@@ -1,8 +1,9 @@
 package com.sales.din.repository;
 
+import com.sales.din.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Product extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
