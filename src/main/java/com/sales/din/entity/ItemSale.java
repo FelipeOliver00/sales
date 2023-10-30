4676052bdf0da0b7cee5cdc421bd8b6994ccb60a
Felipe <felipecastro0208@hotmail.com>
2023-10-30 12:15:05 -0300

    Created new entities class

---
+++ src/main/java/com/sales/din/entity/ItemSale.java
@@ -0,0 +1,29 @@
+package com.sales.din.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "item_sale")
+public class ItemSale {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "sale_id", nullable = false)
+    private Sale sale;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(nullable = false)
+    private int quantity;
+}
