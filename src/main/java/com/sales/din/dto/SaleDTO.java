ec6ba6032f27682f0ddf1eef7c1583339da12511
Felipe <felipecastro0208@hotmail.com>
2023-10-30 16:42:10 -0300

    Created DTO Sale and Product

---
+++ src/main/java/com/sales/din/dto/SaleDTO.java
@@ -0,0 +1,18 @@
+package com.sales.din.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SaleDTO {
+
+    private long userId;
+
+    List<ProductDTO> items;
+
+}
