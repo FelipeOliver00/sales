81571e69a885575304150c435992b7189745cb7b
Felipe <felipecastro0208@hotmail.com>
2023-10-30 11:33:37 -0300

    Created entity class

---
+++ src/main/java/com/sales/din/entity/User.java
@@ -0,0 +1,24 @@
+package com.sales.din.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    private boolean isEnabled;
+
+}
